2c471a8b22f9765c528ebb68595b766d700ede8a
Davide Vernassa <davide.vernassa@gmail.com>
2025-12-10 12:58:57 +0000

    Fisrt homepage commit

--- app/app.vue
+++ app/app.vue
@@ -11,8 +11,8 @@ useHead({
   }
 })
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'Knowage'
+const description = 'The Open Source Analytics and Business Intelligence Suite.'
 
 useSeoMeta({
   title,
