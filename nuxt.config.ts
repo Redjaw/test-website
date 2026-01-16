b06c9187a356caf460f28f2445b147780dfefa3f
dedalus95 <ecerilli1@gmail.com>
2026-01-16 17:39:00 +0100

    chore: add extra nitro config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,17 @@ export default defineNuxtConfig({
     '/': { prerender: true }
   },
 
+  nitro: {
+    prerender: {
+      failOnError: false,
+      crawlLinks: true
+    }
+  },
+
+  image: {
+    domains: ['images.unsplash.com']
+  },
+
   compatibilityDate: '2025-01-15',
 
   eslint: {
